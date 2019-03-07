62e6e24647949e8f6a7790467fde9c583202d53c
Shane Rostad <Shanegrostad@gmail.com>
2019-03-07 06:42:11 -0500

    code review update

--- logic_flow_control/rot13_cipher.js
+++ logic_flow_control/rot13_cipher.js
@@ -1,15 +1,20 @@
 function rot13(string) {
   newString = '';
+  var LOWER_A = 'a'.charCodeAt();
+  var LOWER_M = 'm'.charCodeAt();
+  var LOWER_Z = 'z'.charCodeAt();
+  var UPPER_A = 'A'.charCodeAt();
+  var UPPER_M = 'M'.charCodeAt();
+  var UPPER_Z = 'Z'.charCodeAt();
+
   for (var i = 0; i < string.length; i += 1) {
     charCode = string.charCodeAt(i)
 
-    if (charCode <= 122 && charCode > 109) {
+    if ((charCode <= LOWER_Z && charCode > LOWER_M) ||
+        (charCode <= UPPER_Z && charCode > UPPER_M)) {
       newString += String.fromCharCode(charCode - 13);
-    } else if (charCode <= 109 && charCode >= 97) {
-      newString += String.fromCharCode(charCode + 13);
-    } else if (charCode <= 90 && charCode > 77) {
-      newString += String.fromCharCode(charCode - 13);
-    } else if (charCode <= 77 && charCode >= 65) {
+    } else if ((charCode <= LOWER_M && charCode >= LOWER_A) ||
+               (charCode <= UPPER_M && charCode >= UPPER_A)) {
       newString += String.fromCharCode(charCode + 13);
     } else {
       newString += string[i];
@@ -17,7 +22,6 @@ function rot13(string) {
   }
   return newString;
 }
-
 
 console.log(rot13('Teachers open the door, but you must enter by yourself.'));
 //
